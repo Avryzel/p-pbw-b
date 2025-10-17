121d6f65e513bccbd5f3909b8cfa70d56e25c252
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-10-17 14:33:43 +0700

    Upload tugas pertemuan04

---
+++ pertemuan04/resources/views/kontak.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Selamat Datang di LaraPress</title>
+  </head>
+  <body>
+    <h1>Selamat Datang di Halaman Kontak</h1>
+    <p>Ini adalah halaman Kontak dari aplikasi blog kita.</p>
+    
+    <ul>
+        <li>Nama  : Jel</li>
+        <li>Nomor : 082xxxxx    </li>
+    </ul>
+
+    <a href="/">Kembali ke halaman utama</a>
+  </body>
+</html>
---
+++ pertemuan04/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Selamat Datang di LaraPress</title>
+  </head>
+  <body>
+    <h1>Selamat Datang di Blog LaraPress</h1>
+    <p>Ini adalah halaman utama dari aplikasi blog kita.</p>
+
+    <a href="/tentang-kami">Lihat halaman tentang Kami</a>
+    <a href="/kontak">Lihat halaman kontak</a>
+  
+  </body>
+</html>
